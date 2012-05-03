cdced62be3d9b8e16833ceb714a84a640b271c94
Chris Taylor <crntaylor@gmail.com>
2012-05-03 09:34:52 +0200

    SICP ch1 ex3

--- ch1/ex3.scm
+++ ch1/ex3.scm
@@ -1,7 +1,5 @@
-(def (sum-of-larger-squares a b c)
-    (let (sos x y) (+ (* x x) (* y y))
-        (if (&& (< a b) (< a c)) (sos b c)
-            (&& (< b c) (< b a)) (sos a c)
-                                 (sos a b))))
-
-(sum-of-larger-squares 2 3 4)
+(def sum-of-larger-squares (a b c)
+    (let sos (fn (x y) (+ (* x x) (* y y)))
+        (if (and (< a b) (< a c)) (sos b c)
+            (and (< b c) (< b a)) (sos a c)
+                                  (sos a b))))
